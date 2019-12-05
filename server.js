389e3f4b259786afe565338bd03cc95e9b776566
Leon Machens <leon.machens@googlemail.com>
2019-12-05 10:39:29 +0000

    Add dotenv (#1)

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const MongoClient = require("mongodb").MongoClient;
 
 // Connection URL
-const url = "mongodb://localhost:27017";
+console.log(process.env.MONGO_URL);
+const url = process.env.MONGO_URL;
 
 // Database Name
 const dbName = "intro";
@@ -14,18 +18,8 @@ async function initDb() {
   await client.connect();
   console.log("Connected successfully to server");
   const db = client.db(dbName);
-  const inventoryCollection = db.collection("inventory");
 
-  const cursor = inventoryCollection
-    .find({}, { fields: { tags: false } })
-    .limit(2);
-
-  const inventories = await cursor.toArray();
-  console.log(inventories);
-
-  const inventory = await inventoryCollection.findOne({ item: "canvas" });
-
-  console.log(inventory);
+  db.collection("users").insertOne({ name: "Leon" });
   client.close();
 }
 
