3552ead2ac48bf7f5e4b4f8e069bacb358f7e639
Leon Machens <leon.machens@googlemail.com>
2019-12-05 11:19:44 +0000

    Modulize database (#3)

--- server.js
+++ server.js
@@ -1,35 +1,24 @@
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 const express = require("express");
-const MongoClient = require("mongodb").MongoClient;
+const { initDb, getCollection } = require("./lib/database");
 
 // Connection URL
 const url = process.env.MONGO_URL;
 // Database Name
 const dbName = "intro";
 
-// Create a new MongoClient
-const client = new MongoClient(url);
-
-// Use connect method to connect to the Server
-async function initDb() {
-  await client.connect();
-  console.log("Connected successfully to server");
-  const db = client.db(dbName);
-
-  db.collection("users").insertOne({ name: "Leon" });
-  client.close();
-}
-
-initDb();
-
 const app = express();
 const port = 8081;
 
-app.get("/", (request, response) => {
-  response.send("Hello World");
+app.get("/", async (request, response) => {
+  const user = await getCollection("users").findOne();
+  response.json(user);
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+initDb(url, dbName).then(() => {
+  console.log("Database initialized");
+
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
 });
